22f8851d48ece46be6fa9c265f63c01a49106284
Errol <er.wiley.gh@gmail.com>
2017-12-27 13:48:56 +0000

    updated posts

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
+from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post
 from .forms import PostForm
@@ -9,6 +10,8 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        # message success
+        messages.success(request, "Successfully Created")
         return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         "form": form
@@ -29,7 +32,7 @@ def post_list(request):
         "object_list": queryset,
         "title": "List"
     }
-    return render(request, 'index.html', context)
+    return render(request, 'post_list.html', context)
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
@@ -37,6 +40,7 @@ def post_update(request, id=None):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
         return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         "title": instance.title,
@@ -45,5 +49,8 @@ def post_update(request, id=None):
     }
     return render(request, 'post_form.html', context)
 
-def post_delete(request):
-    return HttpResponse('<h1>delete</h1>')
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Successfully deleted")
+    return redirect("posts:list")
